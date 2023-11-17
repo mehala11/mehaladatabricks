433d644ed24ad3015ff5453c312f9bdb2ed08d9b
mehala11 <mehala11>
2023-11-17 06:53:49 +0000

    done

---
+++ git.sql
@@ -0,0 +1,22 @@
+-- Databricks notebook source
+
+create schema if not exists project;
+use project
+
+-- COMMAND ----------
+
+
+create table if not exists bronze
+
+-- COMMAND ----------
+
+
+COPY INTO bronze
+FROM 'dbfs:/mnt/asadlsad/processeddata/inputproject/json'
+FILEFORMAT = JSON
+FORMAT_OPTIONS('multiline'='True')
+COPY_OPTIONS ('mergeSchema' = 'true')
+
+-- COMMAND ----------
+
+
